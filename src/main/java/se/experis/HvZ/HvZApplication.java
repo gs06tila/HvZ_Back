7d8d25037438027b71c098287b6252ff195d6ea5
gs06tila <tim93larsson@gmail.com>
2019-11-06 16:08:16 +0100

    init

---
+++ src/main/java/se/experis/HvZ/HvZApplication.java
@@ -0,0 +1,13 @@
+package se.experis.HvZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HvZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HvZApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/experis/HvZ/HvZApplicationTests.java
@@ -0,0 +1,11 @@
+package se.experis.HvZ;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HvZApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
