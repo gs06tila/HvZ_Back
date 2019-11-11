9935420245352bdada06f41fb972b718f128b52c
gs06tila <tim93larsson@gmail.com>
2019-11-11 10:44:37 +0100

    expose gameId

---
+++ src/main/java/se/experis/HvZ/domain/RepositoryConfig.java
@@ -0,0 +1,13 @@
+package se.experis.HvZ.domain;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@Configuration
+public class RepositoryConfig  extends RepositoryRestConfigurerAdapter {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Game.class);
+    }
+}
