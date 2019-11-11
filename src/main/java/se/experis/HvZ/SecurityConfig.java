8adba20c9b2128375584f769402f1b9d728a607e
Pär <parso89@gmail.com>
2019-11-11 13:06:25 +0100

    security shit

--- src/main/java/se/experis/HvZ/SecurityConfig.java
+++ src/main/java/se/experis/HvZ/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,25 +31,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(new BCryptPasswordEncoder());
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/api/games");
+    }
+
     // We stipulate that the POST method request to the /login endpoint is allowed without authentication
     // and that requests to all other endpoints require authentication.
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Add this row to allow access to all endpoints
-        http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();
-/*
+       // http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();
+
         http.csrf().disable().cors().and().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
-                .and()
+                    .anyRequest().authenticated()
+                    .and()
                 // Filter for the api/login request
-                .addFilterBefore(new LoginFilter("/login",
+                    .addFilterBefore(new LoginFilter("/login",
                         authenticationManager()),
                         UsernamePasswordAuthenticationFilter.class)
                 // Filter for other requests to check JWT in header
-                .addFilterBefore(new AuthenticationFilter(),
+                    .addFilterBefore(new AuthenticationFilter(),
                         UsernamePasswordAuthenticationFilter.class);
 
- */
     }
 
     //
