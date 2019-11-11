9c50e66b5a0edc68bb20e3dbe229030bf3b1ebf5
Pär <parso89@gmail.com>
2019-11-11 14:10:06 +0100

    test data

--- src/main/java/se/experis/HvZ/SecurityConfig.java
+++ src/main/java/se/experis/HvZ/SecurityConfig.java
@@ -38,13 +38,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/games");
     }
 
+
+
     // We stipulate that the POST method request to the /login endpoint is allowed without authentication
     // and that requests to all other endpoints require authentication.
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // Add this row to allow access to all endpoints
-       // http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();
-
+        http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();
+        /*
         http.csrf().disable().cors().and().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                     .anyRequest().authenticated()
@@ -57,7 +59,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .addFilterBefore(new AuthenticationFilter(),
                         UsernamePasswordAuthenticationFilter.class);
 
+         */
+
     }
+
 
     //
     @Bean
