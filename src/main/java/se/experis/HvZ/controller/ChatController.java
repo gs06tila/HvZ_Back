9dc64d0af5e95750fd86cca95541dc9a12b17b30
gs06tila <tim93larsson@gmail.com>
2019-11-14 09:13:32 +0100

    chat

---
+++ src/main/java/se/experis/HvZ/controller/ChatController.java
@@ -0,0 +1,21 @@
+package se.experis.HvZ.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import se.experis.HvZ.model.ChatMessage;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.register")
+    @SendTo("/topic/public")
+    public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    public ChatMessage
+}
