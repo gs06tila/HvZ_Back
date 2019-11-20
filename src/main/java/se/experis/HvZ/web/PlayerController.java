e4829a470c227db11c21472377bba2c481225e0f
Pär <parso89@gmail.com>
2019-11-20 09:32:47 +0100

    /api/playersdata done

--- src/main/java/se/experis/HvZ/web/PlayerController.java
+++ src/main/java/se/experis/HvZ/web/PlayerController.java
@@ -1,16 +1,12 @@
 package se.experis.HvZ.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import se.experis.HvZ.domain.*;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class PlayerController {
@@ -56,81 +52,32 @@ public class PlayerController {
 
     @PostMapping("/api/playersdata")
     @ResponseBody
-    public ArrayNode getPlayerData(@RequestBody FetchGameInfo fetchGameInfo) {
+    public ArrayList getPlayerData(@RequestBody FetchGameInfo fetchGameInfo) {
 
-        /*
-        ArrayNode playersdata = objectMapper.createArrayNode();
-        ObjectNode playerObject = objectMapper.createObjectNode();
-
-        playerObject.put("key", "value");
-        playerObject.put("key1", "value1");
-        playerObject.put("key2", "value2");
-
-        playersdata.add(playerObject);
-
-        List<Player> l1 = new ArrayList<>();
-        l1 = playerRepository.findAll();
-
-         for(int i =0; i< l1.size(); i++ ){
-             System.out.println(l1.get(i).getPlayerId());
-             //l1.get(i).getPlayerId()
-             //gameUserRepository
-             List<GameUser> gameUsers;
-             //gameUsers = playerRepository.findByGameUser(l1.get(i).getGameUser());
-             //System.out.println(gameUsers.get(0).getUserName());
-         }
-         */
-
-        //int b2 = gameRepository.findById((long) 1).get().getPlayers().size();
-        //System.out.println("b2: " + b2 + "-------------------------------------------------");
-
-        ArrayNode playersData = objectMapper.createArrayNode();
-        ObjectNode playerObject = objectMapper.createObjectNode();
-
-        String userName;
-        userName = gameRepository.findById((long) fetchGameInfo.getGameId()).get().getPlayers().get(0).getGameUser().getUserName();
-        Boolean isHuman;
-        isHuman = gameRepository.findById((long) fetchGameInfo.getGameId()).get().getPlayers().get(0).getHuman();
-        long squadId;
-        squadId = gameRepository.findById((long) fetchGameInfo.getGameId()).get().getPlayers().get(0).getSquadMember().getSquad().getSquadId();
-
-        playerObject.put("userName", userName);
-        playerObject.put("isHuman", isHuman);
-        playerObject.put("squadId", squadId);
-
-        playersData.add(playerObject);
-        playerObject.removeAll();
-
-        playerObject.put("userNams", userName + "s");
-        playerObject.put("isHuman", isHuman);
-        playerObject.put("squadId", squadId);
-
-        playersData.add(playerObject);
-
-
-    /*
-        ArrayNode playersData = objectMapper.createArrayNode();
-        ObjectNode playerObject = objectMapper.createObjectNode();
-
+        ArrayList<ObjectNode> playersData = new ArrayList<>();
         String userName;
         Boolean isHuman;
         long squadId;
 
         int numberOfPlayers = gameRepository.findById((long) 1).get().getPlayers().size();
-        //for(int i=1; i < numberOfPlayers + 1 ; i++){
-            userName = gameRepository.findById((long)fetchGameInfo.getGameId()).get().getPlayers().get(0).getGameUser().getUserName();
-            isHuman = gameRepository.findById((long)fetchGameInfo.getGameId()).get().getPlayers().get(0).getHuman();
-            squadId = gameRepository.findById((long)fetchGameInfo.getGameId()).get().getPlayers().get(0).getSquadMember().getSquad().getSquadId();
+        for(int i=0; i < numberOfPlayers ; i++){
+            ObjectNode playerObject = objectMapper.createObjectNode();
+            userName = gameRepository.findById((long) 1).get().getPlayers().get(i).getGameUser().getUserName();
+            isHuman = gameRepository.findById((long) 1).get().getPlayers().get(i).getHuman();
 
             playerObject.put("userName", userName);
             playerObject.put("isHuman", isHuman);
-            playerObject.put("squadId", squadId);
+
+            if(gameRepository.findById((long) 1).get().getPlayers().get(i).getSquadMember() != null) {
+                squadId = gameRepository.findById((long) 1).get().getPlayers().get(i).getSquadMember().getSquad().getSquadId();
+                playerObject.put("squadId", squadId);
+            }else{
+                playerObject.put("squadId", -1);
+            }
 
             playersData.add(playerObject);
-            playerObject.removeAll();
-      //  }
-        */
 
+       }
         return playersData;
     }
 }
